a4e0a13b1149f511354eb7c3a976d80ec01befc0
Jacob Hoskins <45977627+Jacob-Hoskins@users.noreply.github.com>
2023-10-12 17:24:53 -0400

    Add files via upload
    fresh start

--- listener.py
+++ listener.py
@@ -5,6 +5,9 @@ from bson.json_util import dumps
 from bson.objectid import ObjectId
 import json
 
+connection_string = "mongodb+srv://ietproject17238:52262Jah99.@iet.eiurhrz.mongodb.net/?retryWrites=true&w=majority/"
+client = pymongo.MongoClient(connection_string)
+
 #     test = client.watch([{
 #     '$match': {
 #         'operationType': { '$in': ['insert'] }
@@ -18,87 +21,59 @@ import json
 def find_finished_document(id):
     pass
 
+#function below gets rid of spaces in the list of items to search and start searching on each one
+def reformat_itemList_dictionary(user_info):
+    for y in user_info["items_to_find"]:
+        # x = user_info['items_to_find'][y]
+
+        #TODO: turn into for loop to loop through all the items in the list
+        #Below gives you a string of the item to search
+        preFormat = y["itemName"].split(" ")
+        final_str = ""
+        l = 0
+        while l < len(preFormat) - 1:
+            final_str = final_str + preFormat[l] + "%20"
+            l += 1
+        final = final_str + preFormat[-1]
+
+        #sends item to make_request to start the searching process
+        startSearching.make_request_menards(final)
+
+
+def gather_items_to_search(user_info):
+    # grabs all the users data to pull the info from the db
+    usersInfo = client.get_database("test").get_collection("itemsearches").find_one({'_id': ObjectId(user_info['user_id'])})
+    # adds their search list to our dictionary
+    user_info["items_to_find"] = usersInfo["itemList"]
+    reformat_itemList_dictionary(user_info)
+
+
 def items_to_search(items):
     print(items)
-    for x in items:
-        print(startSearching.make_request(items))
 
-connection_string = "mongodb+srv://ietproject17238:52262Jah99.@iet.eiurhrz.mongodb.net/?retryWrites=true&w=majority/"
 def watch_for_changes():
-    client = pymongo.MongoClient(connection_string)
     test = client.watch()
 
-    itemsToSearch = []
+    searchingInformatoin = {}
 
     for x in test:
         object = json.loads(dumps(x))
         database_change = object['updateDescription']
-        # print(database_change["updatedFields"])
         changed_fields = database_change["updatedFields"]
         if("finsihedEstimate" in changed_fields):
+            # Below gives you ALL the itemList key value pairs, need to find the one by the user who clicked the finished button
             if(changed_fields["finsihedEstimate"] == True):
                 print("Finished Estimate")
-                # TODO: below will give you the finished estimates Document
-                test_id = object["documentKey"]
-                db = client["test"]
-                col = db["itemsearches"]
-                for x in col.find():
-                    # print(x['itemList'])
-
-                    for y in x['itemList']:
-                        itemsToSearch.append(y['itemName'])
-
-                items_to_search(itemsToSearch)
+                userIdStepOne = object['documentKey']
+                # Sets user id of finished estimate below
+                searchingInformatoin["user_id"] = userIdStepOne['_id']['$oid']
+                gather_items_to_search(searchingInformatoin)
             else:
                 print("Nah the estimate isnt finished Bro")
         else:
             print("Not her")
-        # if("finishedEstimate" in database_change["updatedFields"]):
-        #     print("they finished their estimate")
-        # else:
-        #     print("They havent finished their estimate")
 
 
-    # for change in test:
-    #     updated_data = dumps(change)
-    #     print("UPDATED DATA--------", updated_data)
-    #     object = json.loads(updated_data)
-    #     db_info = object["ns"]
-    #     new_collection_data = db_info["coll"]
-    #     # Gets inserted Item
-    #     object_id = (object["documentKey"])
-    #     key_dict = object_id['_id']
-    #     final_key = key_dict["$oid"]
-    #     object_event = (object["updateDescription"])
-    #     grab_field = object_event['updatedFields']
-    #     updated_field = grab_field.get("finishedEstimate")
-    #     print("GRAB FIELD-------", grab_field)
-
-        # if("finishedEstimate" in grab_field):
-        # if (grab_field["finishedEstimate"] == True):
-        #     test = client['test']
-        #     print("They finished their estimate")
-        #     col = test['itemsearches']
-        #     document = col.find_one({"_id": ObjectId(final_key)})
-        #     all_items = startSearching.search_for_items_starting_point(document)
-        #     print(startSearching.make_request())
-        # else:
-        #     print("not finished, different event, check watch_for_changes() to find this message")
-
-
-
-# def watch_for_updates():
-#     client = pymongo.MongoClient(connection_string)
-#     test = client.watch([{
-#         '$match': {
-#             'operationType': {'$in': ['update']}
-#         }
-#     }])
-#     for change in test:
-#         updated_data = dumps(change)
-#         # print(updated_data)
-#         object = json.loads(updated_data)
-#         print(object)
 
 # test = get_database()
 # print(test['test'])
--- startSearching.py
+++ startSearching.py
@@ -1,35 +1,132 @@
+import json
 import os
 import requests
-import bs4
+from bs4 import BeautifulSoup
 
 items_in_search_list = []
-home_depot = 'https://homedepot.com'
-lowes = 'https://www.lowes.com'
+# follow hd with /s/item name to search
+home_depot = 'https://homedepot.com/s/{item}?NCNI-5'
+# follow l with /search?searchTerm= item name to search
+lowes = 'https://www.lowes.com/search?searchTerm={item}'
 # 
-menards = 'https://www.menards.com/search.html?search={item}'
+menards = 'https://www.menards.com/main/search.html?search={item}'
 tileShop = 'https://www.tileshop.com/application/floor'
 
-# URL of site to search goes into the params section
-# TODO: create new headers everytime a search is made to help scrapper reliablity
-def make_request(itemToSearch):
+def create_headers():
+    response = requests.get(
+        url='https://headers.scrapeops.io/v1/browser-headers',
+        params={
+            'api_key': 'a76a84bb-cba1-45f0-a3f0-a234eb6444e2',
+            'num_headers': '2'}
+    )
+    x = response.json()
+    # print(x['result'][1])
+    return x['result'][1]
+
+def make_request_menards(itemToSearch):
     print("IN REQUEST")
-    print(menards.format(item=itemToSearch[0]))
+    final_url = menards.format(item=itemToSearch)
     # response = requests.get('https://www.tileshop.com/application/floor')
-    # response = requests.get(url='https://proxy.scrapeops.io/v1/',
-    # params={
-    #     'api_key': '02192b24-dbdb-456f-95c3-46dcfffdeb40',
-    #     'url': menards,
-    # },)
+    response = requests.get(url='https://proxy.scrapeops.io/v1/',
+    params={
+        'api_key': 'a76a84bb-cba1-45f0-a3f0-a234eb6444e2',
+        'url': final_url,
+        'render_js': True,
+    }, headers=create_headers())
+    soup = BeautifulSoup(response.content, 'html5lib')
+    # print(soup.prettify(), '\n\n -------- \n\n')
+    # print(soup.content, "\n\n---------------------------------------------\n\n")
+    # format_menards(soup.find('div', attrs={'class' : 'pricing-info'}))
+    # print(soup.find('div', attrs={"id": "searchItems"}), "\n\n -------- \n\n")
+    menardsPrice = format_menards(soup.find('div', attrs={"id": "searchItems"}))
+    # print(soup.find('div', attrs={"class": "search-item"}))
+    # print(menardsMainDiv)
+    # print(soup.prettify())
     # print(response.content)
+    #response.content
+    # print(response.content)
+    return menardsPrice
 
-def search_lowes():
+
+
+def make_request_lowes(itemToSearch):
+    testList = []
+    print("IN REQUEST")
+    final_url = lowes.format(item=itemToSearch)
+    response = requests.get(url='https://proxy.scrapeops.io/v1/',
+        params={
+            'api_key': 'a76a84bb-cba1-45f0-a3f0-a234eb6444e2',
+            'url': final_url,
+            'render_js': True,
+        }, headers=create_headers())
+    soup = BeautifulSoup(response.content, 'html5lib')
+    # print(soup.find('div', attrs={'data-selector': 'splp-prd-act-$'}))
+    # test = soup.find('div', attrs={'class': 'prdt-actl-pr'})
+    test = soup.find('div', attrs={'class': 'tile_group'})
+    contentSection= soup.find('section', attrs={'id': 'listItems'})
+    # print(contentSection.find('div', attrs={'data-selector': 'splp-prd-act-$'}))
+    for x in contentSection:
+        testList.append(x)
+
+    print(test)
+
+    # for x in test:
+    #     print(x)
+
+def format_lowes(priceDiv):
     pass
 
-def search_menards():
-    pass
+def format_menards(priceDiv):
+    # print(priceDiv, '\n\n-----\n\n')
+    listOfResultItems = []
+    firstItemPrice = []
 
-def search_home_depot():
-    pass
+    if(priceDiv != None):
+        # print(priceDiv, '\n\n ----- \n\n', list(priceDiv))
+        new_type = str(priceDiv)
+        new_list = new_type.split("</")
+        for x in new_list:
+            if('heading' in x):
+                menardsItemName = x.split('>')[-1]
+                listOfResultItems.append(menardsItemName)
+            if('pricing-info' in x):
+                menardsItemPrice = x.split('\n')[-2]
+                final_price_form = menardsItemPrice.split(' ')[-1]
+                listOfResultItems.append(final_price_form)
+
+        firstItemPrice.append({'itemName': listOfResultItems[0], 'itemPrice': listOfResultItems[1]})
+        # print(listOfResultItems[0], listOfResultItems[1])
+        return firstItemPrice[0]
+        # return listOfResultItems
+
+    elif(priceDiv == None):
+        #TODO: in future a dictionary will be passed along the funcs and will have the item being searched that should be used
+        # here and to restart the search process
+        print("NoneType Error, couldnt properly receieve response")
+
+
+def search_home_depot(itemToSearch):
+    print("IN REQUEST")
+    final_url = home_depot.format(item=itemToSearch)
+    response = requests.get(url='https://proxy.scrapeops.io/v1/',
+        params={
+            'api_key': 'a76a84bb-cba1-45f0-a3f0-a234eb6444e2',
+            'url': final_url,
+            'render_js': True,
+        }, headers=create_headers())
+    soup = BeautifulSoup(response.content, 'html5lib')
+    soupContent = soup.find('section', attrs={'id': 'browse-search-pods-1'})
+    # print(soupContent)
+    for x in soupContent:
+        unformattedNames = x.find('div', attrs={'data-component': 'ProductHeader'})
+        print(unformattedNames)
+        #Below gets prices
+        unformattedPrices = x.find('div', attrs={'class': 'price'})
+        test = str(unformattedPrices)
+        testToList = test.split("</")
+        firstNumber = testToList[1].split(">")
+        secondNumber = testToList[3].split(">")
+        shownPrice = firstNumber[-1], '.', secondNumber[-1]
 
 def search_for_items_starting_point(doc_info):
     for x in doc_info["itemList"]:
@@ -37,3 +134,7 @@ def search_for_items_starting_point(doc_info):
 
     return items_in_search_list
 
+# make_request_menards("hardwood%20flooring")
+# make_request_lowes("hardwood%20flooring")
+# create_headers()
+search_home_depot("dark%20oak%20hardwood%20flooring")
